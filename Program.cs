01a6e0ff7811fc339f4515e83b82157343df9775
inspiringhobbyist <inspiringhobbyist@gmail.com>
2020-08-03 19:49:01 -0600

    message

--- Program.cs
+++ Program.cs
@@ -1,12 +1,1 @@
-﻿using System;
-
-namespace hello_world
-{
-    class Program
-    {
-        static void Main(string[] args)
-        {
-            Console.WriteLine("Hello World , this is awesome!");
-        }
-    }
-}
+﻿
